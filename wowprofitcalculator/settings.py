4d9aff8fa39105f7d4a2e0eeafffcfb2cdeeeae2
Jack Borah <71891256+JackBorah@users.noreply.github.com>
2022-05-12 16:15:23 -0400

    Connected Django to postgres

--- wowprofitcalculator/settings.py
+++ wowprofitcalculator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,18 @@ WSGI_APPLICATION = 'wowprofitcalculator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'WowProfitCalculator',
+
+        'USER': os.environ["WPC_postgres_username"],
+
+        'PASSWORD': os.environ["WPC_postgres_password"],
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
