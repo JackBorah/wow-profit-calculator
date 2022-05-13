18d09d8e462f5c5e11cc13ce4f438954fd4981c5
Jack Borah <71891256+JackBorah@users.noreply.github.com>
2022-05-12 21:39:48 -0400

    Created, debugged, and migrated models

--- calculator/models.py
+++ calculator/models.py
@@ -31,11 +31,11 @@ class Realm(models.Model):
 
     realmId = models.DecimalField(max_digits=4, decimal_places=0, help_text="Realm specific id", primary_key=True)
     name = models.CharField(max_length=30, help_text="Ex: Illidan, Stormrage, ...")
-    population = models.CharField(max_length=20, help_text="Low, Medium, High, Full", help_text="Low, Medium, High, Full", choices=population_choices)
+    population = models.CharField(max_length=20, help_text="Low, Medium, High, Full", choices=population_choices)
     region = models.CharField(max_length=30, help_text="North America, Europe, ...", choices=region_choices)
     timezone = models.CharField(max_length=40)
     type = models.CharField(max_length=20, help_text="Normal, RP, PVP, PVPRP")
-    connectedRealmId = models.ForeignKey('connectedRealm', on_delete=CASCADE)
+    connectedRealmId = models.ForeignKey('connectedRealmsIndex', on_delete=models.CASCADE)
 
 def __str__(self):
     return self.name
@@ -45,16 +45,16 @@ class connectedRealmsIndex(models.Model):
 
 class auction(models.Model):
     auctionId = models.BigIntegerField(primary_key=True)
-    itemId = models.ForeignKey('items', on_delete=CASCADE)
     name = models.CharField(max_length=60)
     buyout = models.IntegerField()
     quantity = models.DecimalField(max_digits=7, decimal_places=0)
     timestamp = models.DateField(auto_now_add=True)
-    connectedRealmId = models.ForeignKey('connectedRealm')
+    connectedRealmId = models.ForeignKey('connectedRealmsIndex', on_delete=models.CASCADE)
+    item = models.ForeignKey('item',on_delete=models.CASCADE)
     itemBonusList = models.ManyToManyField("itemBonus")
 
 class itemBonus(models.Model):
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     effect = models.CharField(max_length=50)
 
 class professionIndex(models.Model):
@@ -68,20 +68,17 @@ class professionTeir(models.Model):
 
 class recipeCatagory(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
-    professionIndex = models.ForeignKey("professionIndex", on_delete=models.CASCADE)
     professionTeir = models.ForeignKey("professionTeir", on_delete=models.CASCADE)
 
 class recipe(models.Model):
-    product = models.ForeignKey("item", on_delete=models.CASCADE)
+    product = models.ManyToManyField("item")
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-    professionIndex = models.ForeignKey("professionIndex", on_delete=models.CASCADE)
-    professionTeir = models.ForeignKey("professionTeir", on_delete=models.CASCADE)
     recipeCatagory = models.ForeignKey("recipeCatagory", on_delete=models.CASCADE)
-    reagent = models.ManyToManyField("item")
+    mats = models.ManyToManyField("material")
+    
 
 class material(models.Model):
-    recipe = models.ForeignKey("recipe", on_delete=models.CASCADE)
     item = models.ForeignKey("item", on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
@@ -89,7 +86,6 @@ class item(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    recipe = models.ForeignKey("recipe", on_delete=models.CASCADE, null=True)
 
 
 
